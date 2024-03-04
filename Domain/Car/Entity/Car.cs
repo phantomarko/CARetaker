839c1118721ca51025ab748debf8dd74b0a9afa3
phantomarko <phantomarko@gmail.com>
2024-03-04 23:15:53 +0100

    feat: add Car entity and CarPlate VO

---
+++ Domain/Car/Entity/Car.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Car.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Car.Entity;
+
+internal class Car
+{
+    private CarPlate Plate { get; }
+
+    public Car(CarPlate plate)
+    {
+        Plate = plate;
+    }
+}
---
+++ Domain/Car/ValueObject/CarPlate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Car.ValueObject;
+
+internal class CarPlate
+{
+    public string Value { get; init; }
+
+    public CarPlate(string value)
+    {
+        Value = value;
+    }
+}
