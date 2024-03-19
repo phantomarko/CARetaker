433548f7da5b022ae7773e31e7a91904646e80e4
phantomarko <phantomarko@gmail.com>
2024-03-20 00:19:46 +0100

    feat: add first migration to create vehicles table

--- Infrastructure/Persistence/ApplicationContext.cs
+++ Infrastructure/Persistence/ApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Vehicles;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence;
 
@@ -6,5 +7,12 @@ public class ApplicationContext : DbContext
 {
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
     {
+    }
+
+    public DbSet<Vehicle> Vehicles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
     }
 }
---
+++ Infrastructure/Persistence/Configurations/VehicleConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Vehicles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
+{
+    private const int NameMaxLength = 50;
+    private const int PlateMaxLength = 16;
+
+    public void Configure(EntityTypeBuilder<Vehicle> builder)
+    {
+        builder
+        .Property(e => e.Name)
+        .HasConversion(
+            v => v.ToString(),
+            v => VehicleName.Create(v))
+        .HasMaxLength(NameMaxLength)
+        .IsRequired();
+
+        builder
+        .Property(e => e.Plate)
+        .HasConversion(
+            v => v.ToString(),
+            v => RegistrationPlate.Create(v))
+        .HasMaxLength(PlateMaxLength)
+        .IsRequired();
+    }
+}
