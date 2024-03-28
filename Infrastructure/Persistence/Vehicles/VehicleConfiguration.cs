7e5531e2e19402f06997737538a1bd12c1709573
phantomarko <phantomarko@gmail.com>
2024-03-29 00:41:46 +0100

    feat: add max length to vehicle name and plate

--- Infrastructure/Persistence/Vehicles/VehicleConfiguration.cs
+++ Infrastructure/Persistence/Vehicles/VehicleConfiguration.cs
@@ -6,8 +6,8 @@ namespace Infrastructure.Persistence.Vehicles;
 
 public sealed class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
 {
-    private const int NameMaxLength = 50;
-    private const int PlateMaxLength = 16;
+    private const int NameMaxLength = 100;
+    private const int PlateMaxLength = 20;
 
     public void Configure(EntityTypeBuilder<Vehicle> builder)
     {
@@ -16,6 +16,7 @@ public sealed class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
         .HasConversion(
             v => v.Value,
             v => VehicleName.Create(v))
+        .HasMaxLength(NameMaxLength)
         .IsRequired();
 
         builder
@@ -23,6 +24,7 @@ public sealed class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
         .HasConversion(
             v => v.Value,
             v => RegistrationPlate.Create(v))
+        .HasMaxLength(PlateMaxLength)
         .IsRequired();
     }
 }
