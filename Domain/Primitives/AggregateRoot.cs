9dae36fd6ed225ef82e13cef15f3c68364a60592
phantomarko <32904502+phantomarko@users.noreply.github.com>
2024-04-02 12:37:28 +0200

    feat: add abstract AggregateRoot with timestamps (#26)

---
+++ Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Primitives;
+
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot(Guid id) : base(id)
+    {
+        CreatedAt = DateTime.UtcNow;
+        RefreshLastUpdate();
+    }
+
+    public DateTime CreatedAt { get; private init; }
+
+    public DateTime UpdatedAt { get; private set; }
+
+    protected void RefreshLastUpdate()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
