fa7f9dc14ca9284926a013183e25a1755be5ec72
phantomarko <phantomarko@gmail.com>
2024-06-14 16:52:22 +0200

    refactor: move date VOs to SharedKernel

--- Domain/Maintenances/Exceptions/LogTimestampFormatIsInvalidException.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Domain.Maintenances.Exceptions;
-
-public sealed class LogTimestampFormatIsInvalidException()
-    : System.Exception("The log timestamp format is invalid");
---
+++ SharedKernel/Dates/BaseDateTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace SharedKernel.Dates;
+
+public abstract record BaseDateTime
+{
+    protected BaseDateTime(DateTime value) => Value = value;
+
+    private DateTime Value { get; }
+
+    protected string Format(string format) => Value.ToString(format);
+
+    protected static DateTime CreateFromFormat(string value, string format)
+    {
+        return DateTime.ParseExact(value, format, CultureInfo.InvariantCulture);
+    }
+}
